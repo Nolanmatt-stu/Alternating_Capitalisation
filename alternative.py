3a5948c0b05974c38788da73518cb97bb0cf3735
Matthew Nolan <133678588+Nolanmatt-stu@users.noreply.github.com>
2024-03-10 17:28:25 +0000

    Update alternative.py
    Removed the counter and amended the for loop if/else statement to work without it.
    Additionally removed comments at bottom of code

--- alternative.py
+++ alternative.py
@@ -24,22 +24,16 @@ user_string_split = user_string.split()
 # This time we're using a list instead of a string to contain the altered case words
 alternating_word_string = []
 
-#The counter is to allow us to alternate the case odd and even numbers 
-counter = 0
-
 for word in user_string_split:
     
-    # If even, the upper case version of the word in the current iteration of the loop will be added to our blank list, and lower case for an odd number
-    if counter % 2 == 0:
+    # If even, the upper case version of the word in the current iteration of the loop will be added to our blank list
+    if user_string_split.index(word) % 2 == 0:
         alternating_word_string.append(word.upper())
     else:
         alternating_word_string.append(word.lower())
-    counter += 1
+    
 
 # The list is then converted back into a string with a space in between each word/item
 final_string = " ".join(alternating_word_string)
 print(final_string)
 
-# I appreciate there is probably a way of doing this without the counter on part two, making it even neater
-# However I couldn't figure out how to find the index number of "word" in each iteration of the "for" loop in order to divide it by 2
-# If that is simple enough to explain in the feedback, I would love to know, thank you!
